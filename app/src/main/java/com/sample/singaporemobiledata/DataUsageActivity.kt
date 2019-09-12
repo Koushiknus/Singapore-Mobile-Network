60dc6c61cbaa4f867098f0c28de7417bfadea0d9
Radhakrisnan Koushik <radhakrisnan.koushik@cartrack.com>
2019-09-12 22:38:29 +0800

    New : Initial Project Creation

---
+++ app/src/main/java/com/sample/singaporemobiledata/DataUsageActivity.kt
@@ -0,0 +1,12 @@
+package com.sample.singaporemobiledata
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DataUsageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_usage)
+    }
+}
