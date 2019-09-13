4c4957a7babee20ae7127d0bf65650842bfeb996
Radhakrisnan Koushik <radhakrisnan.koushik@cartrack.com>
2019-09-13 23:08:35 +0800

    New : View Model and Repository added

---
+++ app/src/main/java/com/sample/singaporemobiledata/datausage/repository/DataUsageRepository.kt
@@ -0,0 +1,8 @@
+package com.sample.singaporemobiledata.datausage.repository
+
+import android.app.Application
+
+class DataUsageRepository(private val mApplication: Application) {
+
+
+}
---
+++ app/src/main/java/com/sample/singaporemobiledata/datausage/viewmodel/DataUsageViewModel.kt
@@ -0,0 +1,8 @@
+package com.sample.singaporemobiledata.datausage.viewmodel
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+
+class DataUsageViewModel(application: Application) : AndroidViewModel(application) {
+    
+}
